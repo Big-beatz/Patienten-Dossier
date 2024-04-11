c02a454397255fb4fc63b21c344998cddbe97d02
ErikKalmaTopicus <119296895+ErikKalmaTopicus@users.noreply.github.com>
2024-04-11 21:30:57 +0200

    Publish project on git

---
+++ src/main/java/com/kalma/Patienten/Dossier/PatientenDossierApplication.java
@@ -0,0 +1,13 @@
+package com.kalma.Patienten.Dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientenDossierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientenDossierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalma/Patienten/Dossier/PatientenDossierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalma.Patienten.Dossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientenDossierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
