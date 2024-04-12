f6af037f52293157e88d429e6ec8efd3a1768d00
ErikKalmaTopicus <119296895+ErikKalmaTopicus@users.noreply.github.com>
2024-04-12 17:54:05 +0200

    PersonController for practising purpose

--- src/main/java/com/kalma/Patienten/Dossier/controllers/HelloController.java
+++ src/main/java/com/kalma/Patienten/Dossier/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.kalma.Patienten.Dossier;
+package com.kalma.Patienten.Dossier.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
---
+++ src/main/java/com/kalma/Patienten/Dossier/models/Person.java
@@ -0,0 +1,42 @@
+package com.kalma.Patienten.Dossier.models;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private LocalDate dob;
+    private char gender;
+    private String hobby;
+
+    public String getHobby(){
+        return this.hobby;
+    }
+
+    public void setHobby(String hobby){
+        this.hobby = hobby;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return this.dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
