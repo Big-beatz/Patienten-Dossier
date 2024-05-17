62e3592447078c012498cdb7df0cb2065a96e8b1
ErikKalmaTopicus <119296895+ErikKalmaTopicus@users.noreply.github.com>
2024-05-18 00:14:11 +0200

    cleanup in exceptionService

--- src/main/java/com/kalma/Patienten/Dossier/Services/ExceptionService.java
+++ src/main/java/com/kalma/Patienten/Dossier/Services/ExceptionService.java
@@ -1,17 +1,11 @@
 package com.kalma.Patienten.Dossier.Services;
 
 import com.kalma.Patienten.Dossier.exceptions.*;
-import com.kalma.Patienten.Dossier.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ExceptionService {
-    private final EmployeeRepository employeeRepository;
-
-    public ExceptionService(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
 
     public void InvalidFunctionException(String message) throws InvalidFunctionException {
         throw new InvalidFunctionException(message);
